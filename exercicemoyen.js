9938e60a1935c7438f65e459a4f43cc6298ff881
najat <nachitnajat@gmail.com>
2024-11-20 10:39:24 +0100

    exercice12

--- exercicemoyen.js
+++ exercicemoyen.js
@@ -1,3 +1,10 @@
+//exercice 11
 let tableau=[1, 2, 3, 4, 5];
 for(let i=0; i<tableau.length; i++)
 {console.log(tableau[i])};
+//exercice 12
+let number=[1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+let numberpair= number.filter(function(pair){
+    return pair %2===0
+});
+console.log(numberpair);
